c1c7382c941e87ccb75bdd15827cc63e8c337aa5
vincent <vincentvanderstruis@icloud.com>
2021-06-10 15:13:56 +0200

    Updates

---
+++ precache-manifest.a6b4a6b12e356b7bb696bfee53899beb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5791b29da976b31b5df7ff4a16999a",
+    "url": "/ether-dice/index.html"
+  },
+  {
+    "revision": "31ecb44bebe396cca8cc",
+    "url": "/ether-dice/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "c68a27f3d662488dd4c5",
+    "url": "/ether-dice/static/css/main.0def4619.chunk.css"
+  },
+  {
+    "revision": "31ecb44bebe396cca8cc",
+    "url": "/ether-dice/static/js/2.36628cbb.chunk.js"
+  },
+  {
+    "revision": "3076bc301cd9ac8a6e5baf44c2b789b6",
+    "url": "/ether-dice/static/js/2.36628cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68a27f3d662488dd4c5",
+    "url": "/ether-dice/static/js/main.9c19a4fe.chunk.js"
+  },
+  {
+    "revision": "bb1d767a18ffa1bcf549",
+    "url": "/ether-dice/static/js/runtime-main.b430fe94.js"
+  },
+  {
+    "revision": "fc8d2280a067dd89ddd7af8dc1662eba",
+    "url": "/ether-dice/static/media/dice.fc8d2280.webp"
+  },
+  {
+    "revision": "53f49b581bd0dea8893033d43f28bd42",
+    "url": "/ether-dice/static/media/dice_logo.53f49b58.png"
+  },
+  {
+    "revision": "531c2648283407880e141af99872ef62",
+    "url": "/ether-dice/static/media/dice_rolling.531c2648.gif"
+  },
+  {
+    "revision": "cd488de327fe726ee152d0f43dce93ff",
+    "url": "/ether-dice/static/media/eth.cd488de3.png"
+  }
+]);
